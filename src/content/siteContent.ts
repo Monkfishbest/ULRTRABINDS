2ea26ff3aeb4c1ce0986ab73ce93c5695508106e
John <shetlandjohnmcneill@hotmail.com>
2026-04-25 11:54:23 +0100

    rejigged text

--- src/content/siteContent.ts
+++ src/content/siteContent.ts
@@ -21,7 +21,8 @@ export const siteContent: SiteContent = {
   companyName: "John's website",
   heroEyebrow: 'Tech solutions and problem solving',
   heroTitle: "Software development and tutoring services",
-  heroBody: "A developer with a broad range of experience including web dev and data engineering and teaching. The roles i've been involved with have ranged from a start-up environment to large corporations and an intensive software development bootcamp as a cohort-lead. I'm open to contract work and tutoring up to advanced higher levels. I also offer coaching for real world practical skills in software development if you're interested in actually relevant skills. I'm also an ex top 5000 Dota 2 player(EU server), sitting around 6.5k MMR now which i'd love to coach people on gaining MMR",
+  heroBody:
+    "I'm a developer with a broad range of experience across web development, data engineering, and teaching. The roles I've been involved with have ranged from start-up environments to large corporations, as well as working as a cohort lead at an intensive software development bootcamp. I'm open to contract work and tutoring up to Advanced Higher level. I also offer coaching in practical, real-world software development skills if you're interested in learning things that actually matter on the job. I'm also an ex top 5000 Dota 2 player on the EU server, sitting around 6.5k MMR now, and I'd be very happy to coach people who want to gain MMR.",
   aboutTitle: "Hi, I'm John",
   aboutBody:
     "I don't quite know what i'm doing with my life, I was working in tech but I got really burnt out, I'm trying to change my relationship with coding and tech to be one that's less associated with being very stressed and anxious so let's call this a sabbatical, if you're seeing this website in it's current state then you're probably a friend, if you are looking for coaching/tutoring then please feel free to contact to me.",
